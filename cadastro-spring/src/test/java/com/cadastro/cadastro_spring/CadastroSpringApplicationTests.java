449cb02498b9faa26870000a71183cfa67fed02b
RafDev88 <Rafaelsousa.dev@hotmail.com>
2026-04-11 20:23:12 -0300

    first commit

---
+++ cadastro-spring/src/test/java/com/cadastro/cadastro_spring/CadastroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro.cadastro_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
