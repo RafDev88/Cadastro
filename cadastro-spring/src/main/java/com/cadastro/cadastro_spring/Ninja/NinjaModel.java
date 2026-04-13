b055884db4a3fed44c06b1119c9925338050f1b3
RafDev88 <Rafaelsousa.dev@hotmail.com>
2026-04-13 16:06:11 -0300

    feat: commit coloca o Lombok nas classes

--- cadastro-spring/src/main/java/com/cadastro/cadastro_spring/Ninja/NinjaModel.java
+++ cadastro-spring/src/main/java/com/cadastro/cadastro_spring/Ninja/NinjaModel.java
@@ -3,6 +3,9 @@ package com.cadastro.cadastro_spring.Ninja;
 
 import com.cadastro.cadastro_spring.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -10,6 +13,9 @@ import java.util.List;
 //GenerationValeu usado para nossa estrategia de como gerar o Id.
 @Entity
 @Table(name="bd_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 
 public class NinjaModel {
     @Id
@@ -19,39 +25,8 @@ public class NinjaModel {
     private String email;
     private int idade;
     @ManyToOne
-    @JoinColumn(name = "missoes_id")//chave estrangeria 
+    @JoinColumn(name = "missoes_id")//chave estrangeria
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
+   
 }
